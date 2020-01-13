f537f73e08c098d833130f2dd7daa353e5c88b5f
Adrien RICHARD <richardadrien0@gmail.com>
2020-01-13 16:02:16 +0100

    improuvements

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from bs4 import BeautifulSoup
-import urllib.request
-import sys
 import ui
 from wikipediaPage import wikipediaPage
+import requests 
 
+game = True
+roundNumber = 0
 randomArticleURl = "https://en.wikipedia.org/wiki/Special:Random"
 
+page1 = wikipediaPage(requests.get(randomArticleURl).url) 
+#page2 = wikipediaPage(requests.get(randomArticleURl).url) 
+print(page1.title.contents[0])
+#print(page2.title.contents[0])
+
 #entry point
+
+#l = wikipediaPage("https://en.wikipedia.org/wiki/Tomorrowland_(festival)")
+#print(l.title)
 ui.startText()
-l = wikipediaPage("https://en.wikipedia.org/wiki/Tomorrowland_(festival)")
-print(l.title)
+page1.printLinkOptions()
+
+while game :
+    ui.roundText(roundNumber)
+    # main loop
+    choice = input("Your choice : ")
+    
+    roundNumber = roundNumber +1
--- wikipediaPage.py
+++ wikipediaPage.py
@@ -8,6 +8,7 @@ class wikipediaPage():
         self.url = url
         self.title = ""
         self.rawpageContent = ""
+        self.allLinksList = []
         # calls the needed methodes
         self.getRawPageContent()
         self.getAllLinks()
@@ -15,7 +16,7 @@ class wikipediaPage():
 
     def getRawPageContent(self):
         req = urllib.request.Request(
-            url="https://en.wikipedia.org/wiki/Tomorrowland_(festival)",
+            url="https://en.wikipedia.org/wiki/Python_(programming_language)",
             headers={'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
         handler = urllib.request.urlopen(req)
         with handler as response:
@@ -25,15 +26,26 @@ class wikipediaPage():
     def getAllLinks(self):
         soup = self.rawpageContent
         # selects all the pragraphs
+        for x in soup.find_all(True, {'class': ['external', 'internal', 'nowrap', 'image', 'navbox', 'infobox_v3', 'infobox_v2', 'infobox_v1', 'infobox']}):
+            x.decompose()
+        for x in soup.find_all(True, {'id': ['toc', 'mw-panel', 'catlinks']}):
+            x.decompose()
         all_links = soup.select('p a[href^="/wiki"]')
         # seests all the table
         all_links.extend(soup.select(
             'table[class^="wikitable"] a[href^="/wiki"]'))
+
         all_links.extend(soup.select('ul a[href^="/wiki"]'))
         all_links.extend(soup.select('ol a[href^="/wiki"]'))
-      #  for p in all_links:
-            #print(p)
-        print(len(all_links))
+        self.allLinksList = all_links
+        # for p in all_links:
+        #     allLinksList.append(p)
+        #     print(p)
+        # print(len(all_links))
 
     def getPageTitle(self):
         self.title = self.rawpageContent.find('h1')
+
+    def printLinkOptions(self):
+        for p in self.allLinksList:
+            print(p.contents[0])
